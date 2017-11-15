fd5ec0d78a93a4bb54920f9af981e7014cbac71d
Dyson1990 <Weaver1990@163.com>
2017-11-15 18:51:08 +0800

    更新上传数据库

--- fund_monitor/spiders/天天基金网_基础数据.py
+++ fund_monitor/spiders/天天基金网_基础数据.py
@@ -14,9 +14,12 @@ import pandas as pd
 import scrapy
 import fund_monitor.items
 import re
+import numpy as np
 import traceback
 import datetime
 import bs4
+import pymysql
+from contextlib import closing
 
 log_path = r'%s/log/spider_DEBUG(%s).log' %(os.getcwd(),datetime.datetime.date(datetime.datetime.today()))
 
@@ -36,8 +39,14 @@ import set_log  # log_obj.debug(文本)  "\x1B[1;32;41m (文本)\x1B[0m"
 log_obj = set_log.Logger(log_path, set_log.logging.WARNING, set_log.logging.DEBUG)
 log_obj.cleanup(log_path, if_cleanup=True) # 是否需要在每次运行程序前清空Log文件
 
+sql = "SELECT `fund_code`, max(`value_date`) as latest_date FROM `eastmoney_daily_data` GROUP BY `fund_code`;"
+
+
 class Spider(scrapy.Spider):
     name = "0001"
+
+    with closing(pymysql.connect('10.10.10.15', 'spider', 'jlspider', 'spider', charset='utf8')) as conn:
+        lastest_date_df = pd.read_sql(sql, conn)
 
     def start_requests(self):
         self.urls = ["http://fund.eastmoney.com/fund.html#os_0;isall_0;ft_;pt_1",]
@@ -71,6 +80,13 @@ class Spider(scrapy.Spider):
         bs_obj = bs4.BeautifulSoup(response.text, 'html.parser')
         item = response.meta['item']
         try:
+            # 读取最新净值日期
+            lastest_date = self.lastest_date_df['latest_date'][self.lastest_date_df['fund_code'] == item['fund_code']]
+            if lastest_date.empty:
+                raise Exception('本地数据库没有找到基金代号%s' %item['fund_code'])
+            lastest_date = lastest_date.iat[0]
+            print "%s的最新净值日期为%s" %(item['fund_code'], lastest_date)
+
             # 净值估算
             e_dl = bs_obj.find('dl', class_='dataItem01')
             data = [e.get_text(strip=True) for e in e_dl.find('dd',class_='dataNums').find_all('span')]
@@ -78,12 +94,34 @@ class Spider(scrapy.Spider):
             data_date = e_dl.find('span', id='gz_gztime').get_text(strip=True)
             ser = pd.Series(data+[data_type,data_date],index=['净值','涨跌值','涨跌幅','数据类型','数据日期'])
             #ser.to_csv('C:\\Users\\Administrator\\Desktop\\test.csv', encoding='utf8',mode='a')
+
             # 基金净值
             e_div = bs_obj.find_all('div', class_='poptableWrap singleStyleHeight01')[0] #有三个标签页，分别是净值，分红，评级
             e_table = e_div.table
-            df = pd.read_html(e_table.prettify(encoding='utf8'), encoding='utf8')[0]
-            #df.to_csv('C:\\Users\\Administrator\\Desktop\\test.csv', encoding='utf8',mode='a')
-            #yield item
+            df = pd.read_html(e_table.prettify(encoding='utf8'), encoding='utf8', header=0)[0]
+
+            year_num = datetime.datetime.now().year
+            df[u'日期'] = pd.to_datetime(df[u'日期'].apply(lambda s:'%s-%s' %(year_num,s)))
+            df = df[df[u'日期']>lastest_date] # 筛选日期
+            #df[u'日期'] = df[u'日期'].apply(lambda date:datetime.datetime.strftime(date,'%Y-%m-%d'))
+            #print dir(df[u'日期'].iat[0])
+            #print df[u'日期'].iat[0]
+
+            df = df.astype(np.str)
+            df[u'key'] = df[u'日期'].apply(lambda date:"%s/%s" %(item['fund_code'], date))
+            df[u'fund_code'] = item['fund_code']
+
+            data_str = ','.join(["(%s)" % (','.join(["%s",] * df.shape[1])) for i in range(df.shape[0])])
+            #data_str = ','.join(["(%s)" %(','.join(l)) for l in np.array(df).tolist()])
+            sql = "INSERT INTO `eastmoney_daily_data`(value_date,net_asset_value,accumulative_net_value,daily_growth_rate,crawler_key,fund_code) VALUES%s;" %data_str
+            #print sql #
+            #print np.array(df).tolist()
+            data_l = []
+            for l in np.array(df).tolist():
+                data_l.extend(l)
+            #print data_l
+            mysql_connecter.connect(sql, args=data_l, host='localhost',user='spider',password = 'jlspider', dbname = 'spider', charset='utf8')
+
         except:
             log_obj.error("%s（ %s ）中无法解析\n%s" % (self.name, response.url, traceback.format_exc()))
             #yield response.meta['item']
